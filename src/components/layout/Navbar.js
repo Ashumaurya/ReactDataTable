2f76d2c825e188b73c9cdd3aa0fe2f51114d2136
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2021-08-03 03:57:28 +0530

    version 1 completed

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,43 @@
 import { AppBar } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
+import { makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: "2px",
+  },
+  title: {
+    flexGrow: 1,
+  },
+});
 
 const Navbar = () => {
+  const classes = useStyles();
+
   return (
-    <div className='navbar'>
+    <div className={classes.root}>
       <div className='navbar-wrapper'>
         <AppBar position='static'>
-          <MenuIcon />
+          <Toolbar>
+            <IconButton
+              edge='start'
+              className={classes.menuButton}
+              color='inherit'
+              aria-label='menu'
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant='h6' className={classes.title}>
+              React Table
+            </Typography>
+          </Toolbar>
         </AppBar>
       </div>
     </div>
